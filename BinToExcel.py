f45c48ca3ec8020d2db66a7d1c5fdec555bffc00
ChunHung LEE <chunhunglee0329@gmail.com>
2016-11-24 00:17:17 +0800

    BinToExcel (3RD commit)

--- BinToExcel.py
+++ BinToExcel.py
@@ -1,6 +1,5 @@
 # coding=UTF-8
 
-#import library.
 import math
 import pkgutil
 import binascii
@@ -9,44 +8,45 @@ import win32com.client
 import os
 from bitstring import BitArray, BitStream
 
+#basic setting:
+FileName = "hexfile.bin" 
+TemplateExcel = "BinToExcel_template.xls"
+OutputExcel = "BinToExcel_result.xls"
+MainPath = "c:" + os.sep + "python27" + os.sep + "exercise" + os.sep + "BintoExcel" + os.sep
 
-def ConvertArrayToString(data):
-	Output=""
-	for string in data:
-		Output += string
-	return Output
-
+#functions
 def GenerateExcel(path, template, data, newfile):
 	#get the path folder to find the excel template. 
 	#Using the data to fill up the excel to save as the new name from newfile
 	xlApp = win32com.client.Dispatch("Excel.Application")
 	workbook=xlApp.Workbooks.Open(os.path.join(path, template))
-	LenData = int(len(data))
-	TotalBits = LenData
-	CurrentBits = 0
-	IndexBits = 0
 	RowIndex = 2
-	while(CurrentBits<TotalBits):
-		IndexBits = int(xlApp.Range("B"+str(RowIndex),"B"+str(RowIndex)).Value)		
-		xlApp.Range("C"+str(RowIndex),"C"+str(RowIndex)).Value = ConvertArrayToString(data[CurrentBits:CurrentBits+IndexBits])
-		CurrentBits = CurrentBits + IndexBits	
+	CurrentBit = 0
+	BitRange = 0
+	while(CurrentBit<int(len(data))):
+		BitRange = int(xlApp.Range("B"+str(RowIndex),"B"+str(RowIndex)).Value)
+		xlApp.Range("C"+str(RowIndex),"C"+str(RowIndex)).Value = data[CurrentBit:CurrentBit+BitRange].bin
+		CurrentBit = CurrentBit + BitRange	
 		RowIndex=RowIndex+1
+	CheckExcelData(int(len(data)), xlApp)
 	workbook.SaveAs(os.path.join(path, newfile))
 	xlApp.DisplayAlerts = 0
 	xlApp.Visible = 0
 	workbook.Close(0)
-	
 
-#new main program
-bitArray = []
-FileName = "hexfile.bin" 
-TemplateExcel = "BinToExcel_template.xls"
-MainPath = "c:" + os.sep + "python27" + os.sep + "exercise" + os.sep + "BintoExcel" + os.sep
-data = file(FileName, 'rb').read()
-Array = BitArray(bytes=data[0:])
-for item in Array:
-	if(item):
-		bitArray.append('1')
-	else:
-		bitArray.append('0')
-GenerateExcel(MainPath, TemplateExcel, bitArray, "BinToExcel_result.xls")
+def CheckExcelData(TotalRows, xlApp):
+	#double check excel data
+	RowIndex = 1
+	ColumnA = ""
+	ColumnB = ""
+	ColumnC = ""
+	while(RowIndex<TotalRows):
+		ColumnA = xlApp.Range("A"+str(RowIndex),"A"+str(RowIndex)).Value
+		ColumnB = xlApp.Range("B"+str(RowIndex),"B"+str(RowIndex)).Value
+		ColumnC = xlApp.Range("C"+str(RowIndex),"C"+str(RowIndex)).Value
+		if(ColumnA=="" or ColumnB=="" or ColumnC==""):
+			print "error"
+			break
+		RowIndex=RowIndex+1
+
+GenerateExcel(MainPath, TemplateExcel, BitArray(bytes=file(FileName, 'rb').read()[0:]), OutputExcel)
