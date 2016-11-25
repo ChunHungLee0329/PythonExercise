b04b822e28b6526f48a6c9e8850678c490326ffc
ChunHung LEE <chunhunglee0329@gmail.com>
2016-11-25 23:18:58 +0800

    BinToExcel (7th commit)

--- BinToExcel.py
+++ BinToExcel.py
@@ -1,5 +1,6 @@
 # coding=UTF-8
 
+import datetime
 import math
 import pkgutil
 import binascii
@@ -11,13 +12,13 @@ from bitstring import BitArray, BitStream
 #basic setting:
 FileName = "hexfile.bin" 
 TemplateExcel = "BinToExcel_template.xls"
-OutputExcel = "BinToExcel_result.xls"
+OutputExcel = "BinToExcel_result"
 MainPath = os.getcwd()  #use getcwd
 GetString=""
 GetArray=[]
 
 #functions	
-def GenerateExcel(path, template, source_type, data, newfile):
+def GenerateExcel(path, template, data, newfile):
 	#get the path folder to find the excel template. 
 	#Using the data to fill up the excel to save as the new name from "newfile"
 	xlApp = win32com.client.Dispatch("Excel.Application")
@@ -26,15 +27,17 @@ def GenerateExcel(path, template, source_type, data, newfile):
 	CurrentBit = 0
 	BitRange = 0
 	while(CurrentBit<int(len(data))):
-		BitRange = int(xlApp.Range("B"+str(RowIndex),"B"+str(RowIndex)).Value)
-		if source_type=="GetArray":
+		BitRange = int(str(xlApp.Range("B"+str(RowIndex),"B"+str(RowIndex)).Value).split(".")[0])
+		#BitRange = int(xlApp.Range("B"+str(RowIndex),"B"+str(RowIndex)).Value)	
+		if type(data) is bitstring.BitArray:
 			xlApp.Range("C"+str(RowIndex),"C"+str(RowIndex)).Value = data[CurrentBit:CurrentBit+BitRange].bin
-		if source_type=="GetString":
+		if type(data) is str:
 			xlApp.Range("C"+str(RowIndex),"C"+str(RowIndex)).Value = data[CurrentBit:CurrentBit+BitRange]			
 		CurrentBit = CurrentBit + BitRange	
 		RowIndex=RowIndex+1
 	CheckExcelData(int(len(data)), xlApp)
-	workbook.SaveAs(os.path.join(path, newfile))
+	strDateTime = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+	workbook.SaveAs(os.path.join(path, newfile+"_"+strDateTime+".xls"))
 	xlApp.DisplayAlerts = 0
 	xlApp.Visible = 0
 	workbook.Close(0)
@@ -58,16 +61,12 @@ def CheckExcelData(TotalRows, xlApp):
 OpenFile=open(FileName, 'rb')
 ReadFile=OpenFile.read();
 GetArray=BitArray(OpenFile,length=256)  #thanks 93mowmow give feedback: Add a hexfile.bin for testing.
+#GetArray=BitArray(bytes=file(FileName, 'rb').read()[0:])
 for element in ReadFile: 
 	GetString = GetString+bin(int(hex(ord(element)), 16))[2:].zfill(8)
 
-GenerateExcel(MainPath, TemplateExcel, "GetArray", GetArray, OutputExcel)
-
-
-
-
-
-
-
+#region main program
+input_data=GetArray #GetString/GetArray
+GenerateExcel(MainPath, TemplateExcel, input_data, OutputExcel)
 
 
