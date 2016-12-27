592597bf350275f35c680ed746529e8305545ef8
Chun-Hung Lee <chunhunglee0329@gmail.com>
2016-12-28 00:14:48 +0800

    DataConverterTool: menu.py (2ND commit)

--- menu.py
+++ menu.py
@@ -10,56 +10,133 @@
 	(1)Version 1.00: create class MainMenu
 '''
 
+
+# -*- coding: utf-8 -*-
+
+''' 檔案資訊
+檔名:menu.py
+版本:version 1.00
+功能:A class to show the ToolUserInterface
+作者:李俊鴻, Neil Lee
+時間:2016/12/26
+更新紀錄:
+	(1)Version 1.00: create class MainMenu
+'''
+
+
 import sys
 from Tkinter import *
 import BinExcelConverter as BEC
+import tkFileDialog, tkMessageBox
 
 
-class MainMenu(Frame):
-	def __init__(self, master):
-		Frame.__init__(self, master, relief=SUNKEN, bd=2, width=400, height=350)
-		self.grid()
-		self.menubar = Menu(self)
-		frame = Frame(master)
-        #frame.pack()	
+class MainMenu():
+	
+	def __init__(self):
+		self.root = Tk() #create an initial Frame 
+		self.root.title("DataConverterTool") #set Window Form Title
+		self.root.geometry("400x350") #set Windows Size
 		
-		#menu: File
-		menuFile = Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="File", menu=menuFile)
+		menubar = Menu(self.root) #default menubar from Menu
+		self.createMenuItem() #create Menu Item
 		
-		#menu: Tool
-		menuTool = Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="Tool", menu=menuTool)
-		menuTool.add_command(label="Bin/Excel Converter", command=self.onBinExcelConverter)
+		#self.textInputPath = self.root.Entry(self.root)
+		#self.textTemplatePath = ""
+		#self.tempInputPath = StringVar()
+	
+	def createMenuItem(self):	
+		#create a Menu object
+		menubar = Menu(self.root)		
 		
-		#menu: Help
-		menuHelp = Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="Help", menu=menuHelp)
-		#menuHelp.add_command(label="About")
-		#menuHelp.add_command(label="User Manual")
+		###Setting MenuItems
+		#1.Menu: "File"
+		menuFile = Menu(menubar, tearoff=0)
+		menubar.add_cascade(label="File", menu=menuFile)		
+		#2.Menu: "Tool"
+		menuTool = Menu(menubar, tearoff=0)
+		menuTool.add_command(label="Bin/Excel Converter", command=self.eventChangeMenu_BinExcelConverter)
+		menubar.add_cascade(label="Tool", menu=menuTool)
+		#3.Menu: "About"
+		menuAbout = Menu(menubar, tearoff=0)
+		menubar.add_cascade(label="About", menu=menuAbout)
+		#4.Menu: "Exit"
+		menuExit = Menu(menubar, tearoff=0)
+		menubar.add_cascade(label="Exit", menu=menuExit)			
+		#show Menu elements
+		self.root.config(menu=menubar)
+		self.root.mainloop()
+	
+	
+	'''eventChangeMenuItem'''
+	def eventChangeMenu_BinExcelConverter(self):
+		#self.MenuChange()
+
+		#frameSource=inputPath section
+		frameSource = Frame(self.root, width=400, height=50, padx=70, pady=10)
+		frameSource.pack(side=TOP, fill=X)
+		lblSource = Label(frameSource, text="Source:").grid(row=0, column=0)
+		textInputPath = Entry(frameSource, bd=1).grid(row=0, column=1)
+		btnSource = Button(frameSource, text="Browse", anchor=W, justify=LEFT, padx=5, command=lambda:textInputPath.set(tkFileDialog.askopenfilename().encode('utf-8'))).grid(row=0, column=2)
+		#btnSource = Button(frameSource, text="Browse", anchor=W, justify=LEFT, padx=5, command=lambda:self.getInputPath()).grid(row=0, column=2)
+		#tkFileDialog.askopenfilename().encode('utf-8')
 		
-		#menu: Exit
-		menuExit = Menu(self.menubar, tearoff=0)
-		self.menubar.add_cascade(label="Exit", menu=menuExit, command=sys.exit)
+		#frameTemplate=templatePath section
+		frameTemplate = Frame(self.root, width=400, height=50, padx=55, pady=10)
+		frameTemplate.pack(side=TOP, fill=X)
+		lblTemplate = Label(frameTemplate, text="Template:").grid(row=0, column=0)
+		self.textTemplatePath = Entry(frameTemplate, bd=1).grid(row=0, column=1)
+		btnTemplate = Button(frameTemplate, text="Browse", anchor=W, justify=LEFT, padx=5, command=lambda:self.getTemplatePath()).grid(row=0, column=2)
 		
-		root.config(menu=self.menubar)
-		root.mainloop()	
+		#frameMessage=Message:
+		frameMessage = Frame(self.root, width=400, height=50, padx=35, pady=10)
+		frameMessage.pack(side=TOP, fill=X)
+		lblMessage = Label(frameMessage, text="Message:", anchor=W, justify=LEFT, padx=20, pady=20).grid(row=2, column=0)
+		#textMessage = ScrolledText(frameMessage, bd=1)
+		#textMessage.pack(row=2, column=1)
 		
-	def onBinExcelConverter(self):
-		root = None
-		self.MenuChange()		
-		#menuHelp = Menu(self.menubar, tearoff=0)
-		#self.menubar.add_cascade(label="Help", menu=menuHelp)
-		#menuHelp.add_command(label="aaa")
-		#menuHelp.add_command(label="User Manual")
-		#print "1234"		
+		#frameConvert=button Convert:
+		frameConvert = Frame(self.root, width=400, height=25, padx=150)
+		frameConvert.pack(side=TOP, fill=X)		
+		btnConverter = Button(frameConvert, text="Converter", command=lambda:self.convert(i, o))
+		btnConverter.grid(row=3, column=1)		
+
 		
-	def MenuChange(self):		
-		app = MainMenu(self)	
+	
+	def getInputPath(self):
+		aaa = "1"
+		#self.tempInputPath.set(tkFileDialog.askopenfilename().encode('utf-8'))
+		#tempInputPath = tkFileDialog.askopenfilename().encode('utf-8')
+		#self.textInputPath.set(tempInputPath)
+	
+	def getTemplatePath(self):
+		tempTemplatePath = tkFileDialog.askopenfilename().encode('utf-8')
+		#self.TemplatePath.set(tempTemplatePath)
+	
+	
+	def convert(self, i, o):
+		self.i = i
+		self.o = o
+		print i	
+	
+	
+	def MenuChange(self):
+		#self.root.destroy()
+		print "1234"
+	
+	
+	
+	
+	
+	
+	
+	
 		
 		
 if __name__ == '__main__':
-	root = Tk()
-	app = MainMenu(root)
-	#app.pack()
-	#root.mainloop()
+	print "5678"
+	
+	
+	
+
+	
+	
