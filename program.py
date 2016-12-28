0304052fc7e65b00cccb8099cdfec75ca8452403
Chun-Hung Lee <chunhunglee0329@gmail.com>
2016-12-28 20:32:24 +0800

    DataConverterTool: main program "program.py" (3ST commit)

--- program.py
+++ program.py
@@ -2,18 +2,21 @@
 
 ''' 檔案資訊
 檔名:program.py
-版本:version 1.00
+版本:version 2.00
 功能:A Tool support data file converter. (Data Converter Tool.ppt)
 作者:李俊鴻, Neil Lee
 時間:2016/12/24
 更新紀錄:
-	(1)Version 1.00:support BinToExcel/ExcelToBin
+	=== Version 1.00 2016/12/26 update ===
+	(1)support BinToExcel/ExcelToBin
+	=== Version 1.01 2016/12/28 update ===
+	(1)add new Menu class "menu_new"
 '''
 
 from Tkinter import *
 import sys
 import os
-import binExcelConverter as BEC
+import BinExcelConverter as BEC
 import menu_new as M_new
 import menu as M
 
@@ -21,20 +24,22 @@ def main():
 
 	help = "sample command for you: python program.py C:\a.bin C:\b.xls"
 	if(len(sys.argv) == 1):	
-		#root = Tk()
-		#myMenu = M_new.MainMenu(root)
-		myMenu = M.MainMenu()
+		root = Tk()
+		myMenu = M_new.MainMenu(root)
+		#myMenu = M.MainMenu()
 		
 	elif(len(sys.argv) == 3):
 		input = sys.argv[1] #get the parameter: inputPath
 		template = sys.argv[2] #get the parameter: outputPath
 		converter = BEC.BinExcelConverter(input, template) #assin an object to "converter" from class "BinExcelConverter"
-		if(not converter.volidateFileExtension()):#volidate file name extension
-			print converter.message()
+		if(not converter.volidateVariable()): #volidate if input/template missing
+			print converter.message
+		elif(not converter.volidateFileExtension()):#volidate file name extension
+			print converter.message
 		elif(not converter.volidateConvertingItem()):#volidate converting item(BinToExcel/ExcelToBin)
-			print converter.message()
+			print converter.message
 		elif(not converter.volidateFilePath()):#volidate file existence (inputPath/templatePath)
-			print converter.message()
+			print converter.message
 		else:
 			converter.copyAsTempData()#copy data into folder=tempData (inputPath/templatePath)
 			converter.readFileToString()#read file data to a string
