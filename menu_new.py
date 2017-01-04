e06defadcc333107e6029d6e772ed7e67889846d
Chun-Hung Lee <chunhunglee0329@gmail.com>
2017-01-04 21:15:22 +0800

    DataConverterTool: menu_new.py (5th commit, new)

--- menu_new.py
+++ menu_new.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 
-''' æª”æ¡ˆè³‡è¨Š
-æª”å:menu.py
-ç‰ˆæœ¬:version 1.00
-åŠŸèƒ½:A class to show the ToolUserInterface
-ä½œè€…:æŽä¿Šé´», Neil Lee
-æ™‚é–“:2016/12/28
-æ›´æ–°ç´€éŒ„:(æœ€æ–°çš„åœ¨æœ€ä¸Šé¢)
+''' ÀÉ®×¸ê°T
+ÀÉ¦W:menu.py
+ª©¥»:version 1.00
+¥\¯à:A class to show the ToolUserInterface
+§@ªÌ:§õ«TÂE, Neil Lee
+®É¶¡:2016/12/28
+§ó·s¬ö¿ý:(³Ì·sªº¦b³Ì¤W­±)
+	=== Version 1.03 2017/1/4 update ===
+	(1)chnage UI design/event: File->open sourcePath/templatePath
+	(2)add config menu to control excel column
+	(3)add login function  ==>pending db link
+	(4)writelog()  ==>pending db link and insert into db.
 	=== Version 1.02 2016/12/29 update ===
 	(1)remove frameMessage in UI.
 	(2)"tkMessageBox" will have message.
@@ -14,7 +19,7 @@
 	(1)add function "writeLog()" to keep operation error message file log.
 	=== Version 1.00 2016/12/26 update ===
 	(1)create class MainMenu	
-ä½œè€…ç­†è¨˜:
+§@ªÌµ§°O:
 	(1)login/logout function is pending.
 	(2)UI design/event change per eMow comment
 	(3)add the menu to load a "Config.ini" setting.
@@ -22,6 +27,7 @@
 
 
 import sys
+import Tkinter
 from Tkinter import *
 import BinExcelConverter as BEC
 import tkFileDialog, tkMessageBox
@@ -29,124 +35,148 @@ import tkFileDialog, tkMessageBox
 
 
 
-class MainMenu(Tk):
-	
-	
-	#initial class
+class MainMenu(Tkinter.Tk):
+	#[section]: initial
 	def __init__(self, root):
+		#initial variable (constructor)
+		self.loginID = ""
+		self.Msg = ""
+		self.sourcePath = ""
+		self.templatePath = ""
+		self.configExcelSetting = ""
+		#setting variable
 		self.root = root
 		self.root.title("DataConverterTool") #set Window Form Title
-		self.root.geometry("500x350") #set Windows Size
+		self.root.geometry("600x400") #set Windows Size
 		self.frame = Frame(root) #top level: frame
-		self.menuBar = Menu(root) #create a default UI Menu object
-		
-		#self.createLoginWindow() #windows to login
+		self.menuBar = Menu(root) #create a default UI Menu object		
 		self.createMenuItem() #loading MenuItem		
-		self.loginID = ""
-	
-	
-	def createLoginWindow(self):
-		#frmLogin = Toplevel()
-		#frmLogin.title("Login ID/Password...")
-		return False
+
 		
-	
-	
+		
+	#[section]: create UI/Menu
 	#create MenuItem: menuBar
 	def createMenuItem(self): 		
-		#1.Menu: "File"
+		#1. Menu:"File"
 		self.menuFile = Menu(self.menuBar, tearoff=0)
+		self.menuFile.add_command(label="Source File", command=lambda:self.browseFilePath(label="sourcePath"))
+		self.menuFile.add_command(label="Template File", command=lambda:self.browseFilePath(label="templatePath"))
+		self.menuFile.add_separator()
+		self.menuFile.add_command(label="Login", command=self.createLoginWindow)
+		self.menuFile.add_command(label="Logout", command=self.logout)
+		self.menuFile.add_separator()
+		self.menuFile.add_command(label="Exit", command=lambda:sys.exit())
 		self.menuBar.add_cascade(label="File", menu=self.menuFile)
-		
-		#2.Menu: "Tool"		
+		#2. Menu:"Tool"		
 		self.menuTool = Menu(self.menuBar, tearoff=0)
-		#2.1 "Bin/Excel Converter"
-		self.menuTool.add_command(label="Bin/Excel Converter", command=self.eventChangeMenuItem_BinExcelConverter) 
+		self.menuTool.add_command(label="Bin/Excel Converter", command=self.eventChangeMenuItem_Tool_BinExcelConverter)
+		self.menuTool.add_command(label="others...") 
 		self.menuBar.add_cascade(label="Tool", menu=self.menuTool)
-		
-		#3.Menu: "About"
-		self.menuAbout = Menu(self.menuBar, tearoff=0)
-		self.menuBar.add_cascade(label="About", menu=self.menuAbout)
-		
-		#4.Menu: "Exit"
-		self.menuExit = Menu(self.menuBar, tearoff=0)
-		#4.1 "Exit"
-		self.menuExit.add_command(label="Exit", command=lambda:sys.exit())
-		self.menuBar.add_cascade(label="Exit", menu=self.menuExit) 
-		
-		#show Menu elements
+		#3. Menu:"Config"
+		self.menuConfig = Menu(self.menuBar, tearoff=0)
+		#3.1 Menu:"Config"-->"ExcelSetting"
+		self.menuConfigSubmenu = Menu(self.menuConfig, tearoff=0)
+		self.menuConfigSubmenu.add_radiobutton(label="ExcelSetting_Default", command=lambda:self.eventChhangeMenuItem_Config_ExcelSetting(item="ExcelSetting_Default"))
+		self.menuConfigSubmenu.add_radiobutton(label="ExcelSetting_+1", command=lambda:self.eventChhangeMenuItem_Config_ExcelSetting(item="ExcelSetting_+1"))
+		self.menuConfigSubmenu.add_radiobutton(label="ExcelSetting_+2", command=lambda:self.eventChhangeMenuItem_Config_ExcelSetting(item="ExcelSetting_+2"))
+		self.menuConfig.add_cascade(label='ExcelSetting', menu=self.menuConfigSubmenu, underline=0)
+		#3.2 Menu:"Config"-->OtherConfig
+		self.menuConfig.add_command(label="OtherConfig...", underline=0)
+		self.menuBar.add_cascade(label="Config", underline=0, menu=self.menuConfig)	
+		#show Menu elements		
 		self.root.config(menu=self.menuBar)
 		self.frame.pack()
 		self.root.mainloop()
-
-	
-	#browse to assign a file path
-	def browseFilePath(self):
-		return tkFileDialog.askopenfilename().encode('utf-8')
-
-		
-	#select Menu: "Tool"->"Bin/Excel Converter"
-	def refreshMenuChange(self):
-		self.frame.grid_remove()
-	
-		
-	#eventChangeMenu: go to Menu "Tool" -> "Bin/Excel Converter"
-	def eventChangeMenuItem_BinExcelConverter(self):
-		#self.refreshMenuChange()
-		
-		#frameSource: a Frame layout to clone "inputPath section"
-		self.tempInput = StringVar() #string variable:"tempInput", assign value into "textInputPath"
-		self.frameSource = Frame(self.frame, pady=5)
-		self.frameSource.pack(side=TOP, fill=X)
-		self.lblSource = Label(self.frameSource, text="Source:", width=10).grid(row=0, column=0)
-		self.textInputPath = Entry(self.frameSource, width=40, state="readonly", textvariable=self.tempInput).grid(row=0, column=1)
-		self.btnSource = Button(self.frameSource, text="Browse", width=8, command=lambda:self.tempInput.set(self.browseFilePath())).grid(row=0, column=2)
-
-		#frameTemplate: a Frame layout to clone "templatePath section"
-		self.tempTemplate = StringVar() #string variable:"tempTemplate", assign value into "textTemplatePath"
-		self.frameTemplate = Frame(self.frame, pady=5)
-		self.frameTemplate.pack(side=TOP, fill=X)
-		self.lblTemplate = Label(self.frameTemplate, text="Template:", width=10).grid(row=0, column=0)
-		self.textTemplatePath = Entry(self.frameTemplate, width=40, state="readonly", textvariable=self.tempTemplate).grid(row=0, column=1)
-		self.btnTemplate = Button(self.frameTemplate, text="Browse", width=8, command=lambda:self.tempTemplate.set(self.browseFilePath())).grid(row=0, column=2)
-		
-		'''
-		#frameMessage: a Frame layout to clone "Message section"
-		self.tempMessage = StringVar() #string variable:"tempMessage", assign value into "textMessage"
-		self.frameMessage = Frame(self.frame, pady=15)
-		self.frameMessage.pack(side=TOP, fill=X)		
-		self.lblMessage = Label(self.frameMessage, text="Message:", width=10).grid(row=0, column=0)
-		self.textMessage = Text(self.frameMessage, width=48, height=5, state='disabled').grid(row=0, column=1)
-		#self.textMessage = Listbox(self.frameMessage, bd=1, width=48, height=3).grid(row=0, column=1)
-		'''
-		
-		#frameConvert= a Frame layout to clone "Convert Button section"
-		self.frameConvert = Frame(self.frame, pady=5)
-		self.frameConvert.pack(side=TOP, fill=X)		
-		self.btnConverter = Button(self.frameConvert, text="Converter", command=lambda:self.convert(self.tempInput.get(), self.tempTemplate.get()))
-		self.btnConverter.grid(row=3, column=1, padx=200)
-		
-		#frame grid
-		self.frame.grid(row=0, column=0, padx=15, pady=30)
 	
 	
-	#execute Data Convertion
-	def convert(self, input, template):
-		converter = BEC.BinExcelConverter(input, template)		
-		if(not converter.validationEncapsulation()):
-			converter.writeLog()
-			tkMessageBox.showinfo("Error", converter.message)
-		else:			
-			converter.convertingEncapsulation()#start converting to a new file.
-			tkMessageBox.showinfo("OK", converter.message)
-		
 	
+	#[section]: Login/Logout template
+	#System Login/Logout function
+	def createLoginWindow(self):
+		#open window: login
+		self.frmLogin = Toplevel(self.root)
+		self.frmLogin.title("login: enter your info.")
+		self.frmLogin.geometry("250x150")
+		self.frameLogin = Frame(self.frmLogin) #create a Frame "frameLogin" into fraLogin
+		#frameLoginID: a Frame layout to clone "LoginID section"
+		self.tempLoginID = StringVar() #string variable:"tempLoginID", assign value into "textLoginID"
+		self.frameLoginID = Frame(self.frameLogin, pady=12)
+		self.frameLoginID.pack(side=TOP, fill=X)
+		self.lblLoginID = Label(self.frameLoginID, text="UserID:", width=10).grid(row=0, column=0)
+		self.textLoginID = Entry(self.frameLoginID, width=20, textvariable=self.tempLoginID).grid(row=0, column=1)
+		#frameLoginPWD: a Frame layout to clone "LoginPWD section"
+		self.tempLoginPWD = StringVar() #string variable:"tempLoginPWD", assign value into "textLoginPWD"
+		self.frameLoginPWD = Frame(self.frameLogin, pady=12)
+		self.frameLoginPWD.pack(side=TOP, fill=X)
+		self.lblLoginPWD = Label(self.frameLoginPWD, text="Password:", width=10).grid(row=0, column=0)
+		self.textLoginPWD = Entry(self.frameLoginPWD, width=20, show="*", textvariable=self.tempLoginPWD).grid(row=0, column=1)
+		#frameLoginBTN: a Frame layout to clone "LoginBTN section"
+		self.frameLoginBTN = Frame(self.frameLogin)
+		self.frameLoginBTN.pack(side=TOP, fill=X)
+		self.btnLogin = Button(self.frameLoginBTN, text="Login", command=self.login)
+		self.btnLogin.grid(row=0, column=0, padx=100)
+		#frame pack/mainloop
+		self.frameLogin.pack()
+		self.frmLogin.mainloop()
+	#Login	
 	def login(self):
-		print ""
-
+		#hardcode ID/PWD:1234/1234
+		self.frmLogin.destroy()
+		if(self.tempLoginID.get()=="1234" and self.tempLoginPWD.get()=="1234"):
+			self.loginID = self.tempLoginID
+			self.Msg = "Login successfully!"
+			tkMessageBox.showinfo("OK", self.Msg)
+		else:
+			self.loginID = ""
+			self.Msg = "Try again!"
+			tkMessageBox.showinfo("Error", self.Msg)
+	#Logout
 	def logout(self):
-		print ""
+		self.loginID = ""
+		self.Msg = "Logout successfully!"
+		tkMessageBox.showinfo("OK", self.Msg)
 		
+	
+	
+	#[section]: open Files dialog
+	#File open
+	def browseFilePath(self, label):
+		tempPath = tkFileDialog.askopenfilename().encode('utf-8')
+		setattr(self, label, tempPath)
+		self.Msg = "Upload " + label + " successfully below:\n" + tempPath
+		tkMessageBox.showinfo("OK", self.Msg)
+		
+		
+		
+	#[section]: Menu "Tool" change event 
+	#Bin/Excel Converter
+	def eventChangeMenuItem_Tool_BinExcelConverter(self):
+		if(self.loginID == ""):
+			tkMessageBox.showinfo("Error", "Please login system firstly!")
+		else:
+			converter = BEC.BinExcelConverter(self.sourcePath, self.templatePath)
+			#OverRide object("converter") attribute
+			if(self.configExcelSetting is not ""):
+				converter.configExcelSetting = self.configExcelSetting
+				converter.defaultProgramSetting()
+			#start converter
+			if(not converter.validationEncapsulation()):
+				converter.writeLog()
+				tkMessageBox.showinfo("Error", converter.message)
+			else:			
+				converter.convertingEncapsulation()#start converting to a new file.
+				tkMessageBox.showinfo("OK", converter.message)
+	
+	
+	
+	#[section]: Menu "Config" change event
+	#"ExcelSetting"
+	def eventChhangeMenuItem_Config_ExcelSetting(self, item):
+		self.configExcelSetting = item
+		
+	
+	
+
 	
 
 	
@@ -157,3 +187,4 @@ if __name__ == '__main__':
 	app = MainMenu(root)
 	app.pack()
 	root.mainloop()
+	
