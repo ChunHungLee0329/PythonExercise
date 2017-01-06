42345232591d9c491c3ffdee99e00f30d5abe113
Chun-Hung Lee <chunhunglee0329@gmail.com>
2017-01-06 23:55:08 +0800

    DataConverterTool: main program "program.py" (6th commit)

--- program.py
+++ program.py
@@ -2,11 +2,14 @@
 
 ''' 檔案資訊
 檔名:program.py
-版本:version 2.00
+版本:version 1.02
 功能:A Tool support data file converter. (Data Converter Tool.ppt)
 作者:李俊鴻, Neil Lee
 時間:2016/12/24
 更新紀錄:(最新的在最上面)
+	=== Version 1.02 2016/1/5 update ===
+	(1)use class "menu" from file "menu.py"
+	(2)use class "BinExcelConverter" from file "BinExcelConverter.py"
 	=== Version 1.01 2016/12/28 update ===
 	(1)add new Menu class "menu_new"
 	=== Version 1.00 2016/12/26 update ===
@@ -16,22 +19,22 @@
 from Tkinter import *
 import sys
 import os
-import BinExcelConverter_final as BEC
-import menu_new as M_new
-#import menu as M
+import BinExcelConverter as BEC
+#import menu_new as M_new
+import menu as M
 
 def main():	
 
 	help = "sample command for you: python program.py C:\a.bin C:\b.xls"
 	if(len(sys.argv) == 1):	
 		root = Tk()
-		myMenu = M_new.MainMenu(root)
-		#myMenu = M.MainMenu()
+		#myMenu = M_new.MainMenu(root)
+		myMenu = M.MainMenu(root)
 		
 	elif(len(sys.argv) == 3):
 		input = sys.argv[1] #get the parameter: inputPath
 		template = sys.argv[2] #get the parameter: outputPath
-		converter = BEC.BinExcelConverter(input, template) #assin an object to "converter" from class "BinExcelConverter"
+		converter = BEC.BinExcelConverter(input, template) #assign an object to "converter" from class "BinExcelConverter"
 		if(not converter.validationEncapsulation()):
 			converter.writeLog()
 			print converter.message
