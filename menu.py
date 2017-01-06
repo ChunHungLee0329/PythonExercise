8c888734dae9dcd333f4df7b9abb2167193a0adf
Chun-Hung Lee <chunhunglee0329@gmail.com>
2017-01-06 23:55:34 +0800

    DataConverterTool: menu.py (3rd commit)

--- menu.py
+++ menu.py
@@ -1,142 +1,212 @@
 # -*- coding: utf-8 -*-
 
-''' æª”æ¡ˆè³‡è¨Š
-æª”å:menu.py
-ç‰ˆæœ¬:version 1.00
-åŠŸèƒ½:A class to show the ToolUserInterface
-ä½œè€…:æŽä¿Šé´», Neil Lee
-æ™‚é–“:2016/12/26
-æ›´æ–°ç´€éŒ„:
-	(1)Version 1.00: create class MainMenu
-'''
-
-
-# -*- coding: utf-8 -*-
-
-''' æª”æ¡ˆè³‡è¨Š
-æª”å:menu.py
-ç‰ˆæœ¬:version 1.00
-åŠŸèƒ½:A class to show the ToolUserInterface
-ä½œè€…:æŽä¿Šé´», Neil Lee
-æ™‚é–“:2016/12/26
-æ›´æ–°ç´€éŒ„:
-	(1)Version 1.00: create class MainMenu
+''' ÀÉ®×¸ê°T
+ÀÉ¦W:menu.py
+ª©¥»:version 1.03
+¥\¯à:A class to show the ToolUserInterface
+§@ªÌ:§õ«TÂE, Neil Lee
+®É¶¡:2016/12/28
+§ó·s¬ö¿ý:(³Ì·sªº¦b³Ì¤W­±)
+	=== Version 1.03 2017/1/4 update ===
+	(1)chnage UI design/event: File->open sourcePath/templatePath
+	(2)add config menu to control excel column
+	(3)add login function  ==>pending db link
+	(4)writelog()  ==>pending db link and insert into db.
+	=== Version 1.02 2016/12/29 update ===
+	(1)remove frameMessage in UI.
+	(2)"tkMessageBox" will have message.
+	=== Version 1.01 2016/12/28 update ===
+	(1)add function "writeLog()" to keep operation error message file log.
+	=== Version 1.00 2016/12/26 update ===
+	(1)create class MainMenu	
+§@ªÌµ§°O:
+	(1)login/logout function is pending.
+	(2)UI design/event change per eMow comment
+	(3)add the menu to load a "Config.ini" setting.
 '''
 
 
 import sys
+import Tkinter
 from Tkinter import *
 import BinExcelConverter as BEC
+import DatabaseConnector as DBC
 import tkFileDialog, tkMessageBox
+import os
 
 
-class MainMenu():
-	
-	def __init__(self):
-		self.root = Tk() #create an initial Frame 
+
+class MainMenu(Tkinter.Tk):
+	#[section]: initial
+	def __init__(self, root):
+		#initial variable (constructor)
+		self.loginID = ""
+		self.Msg = ""
+		self.sourcePath = ""
+		self.templatePath = ""
+		self.configExcelSetting = ""
+		self.menuAction = ""
+		#setting variable
+		self.root = root
 		self.root.title("DataConverterTool") #set Window Form Title
-		self.root.geometry("400x350") #set Windows Size
+		self.root.geometry("600x400") #set Windows Size
+		self.frame = Frame(root) #top level: frame
+		self.menuBar = Menu(root) #create a default UI Menu object		
+		self.createMenuItem() #loading MenuItem		
+
 		
-		menubar = Menu(self.root) #default menubar from Menu
-		self.createMenuItem() #create Menu Item
 		
-		#self.textInputPath = self.root.Entry(self.root)
-		#self.textTemplatePath = ""
-		#self.tempInputPath = StringVar()
-	
-	def createMenuItem(self):	
-		#create a Menu object
-		menubar = Menu(self.root)		
-		
-		###Setting MenuItems
-		#1.Menu: "File"
-		menuFile = Menu(menubar, tearoff=0)
-		menubar.add_cascade(label="File", menu=menuFile)		
-		#2.Menu: "Tool"
-		menuTool = Menu(menubar, tearoff=0)
-		menuTool.add_command(label="Bin/Excel Converter", command=self.eventChangeMenu_BinExcelConverter)
-		menubar.add_cascade(label="Tool", menu=menuTool)
-		#3.Menu: "About"
-		menuAbout = Menu(menubar, tearoff=0)
-		menubar.add_cascade(label="About", menu=menuAbout)
-		#4.Menu: "Exit"
-		menuExit = Menu(menubar, tearoff=0)
-		menubar.add_cascade(label="Exit", menu=menuExit)			
-		#show Menu elements
-		self.root.config(menu=menubar)
+	#[section]: create UI/Menu
+	#create MenuItem: menuBar
+	def createMenuItem(self): 		
+		#1. Menu:"File"
+		self.menuFile = Menu(self.menuBar, tearoff=0)
+		self.menuFile.add_command(label="Source File", command=lambda:self.browseFilePath(label="sourcePath"))
+		self.menuFile.add_command(label="Template File", command=lambda:self.browseFilePath(label="templatePath"))
+		self.menuFile.add_separator()
+		self.menuFile.add_command(label="Login", command=self.createLoginWindow)
+		self.menuFile.add_command(label="Logout", command=self.logout)
+		self.menuFile.add_separator()
+		self.menuFile.add_command(label="Exit", command=lambda:sys.exit())
+		self.menuBar.add_cascade(label="File", menu=self.menuFile)
+		#2. Menu:"Tool"		
+		self.menuTool = Menu(self.menuBar, tearoff=0)
+		self.menuTool.add_command(label="Bin/Excel self.converter", command=self.eventChangeMenuItem_Tool_BinExcelConverter)
+		self.menuTool.add_command(label="others...") 
+		self.menuBar.add_cascade(label="Tool", menu=self.menuTool)
+		#3. Menu:"Config"
+		self.menuConfig = Menu(self.menuBar, tearoff=0)
+		#3.1 Menu:"Config"-->"ExcelSetting"
+		self.menuConfigSubmenu = Menu(self.menuConfig, tearoff=0)
+		self.menuConfigSubmenu.add_radiobutton(label="ExcelSetting_Default", command=lambda:self.eventChhangeMenuItem_Config_ExcelSetting(item="ExcelSetting_Default"))
+		self.menuConfigSubmenu.add_radiobutton(label="ExcelSetting_+1", command=lambda:self.eventChhangeMenuItem_Config_ExcelSetting(item="ExcelSetting_+1"))
+		self.menuConfigSubmenu.add_radiobutton(label="ExcelSetting_+2", command=lambda:self.eventChhangeMenuItem_Config_ExcelSetting(item="ExcelSetting_+2"))
+		self.menuConfig.add_cascade(label='ExcelSetting', menu=self.menuConfigSubmenu, underline=0)
+		#3.2 Menu:"Config"-->OtherConfig
+		self.menuConfig.add_command(label="OtherConfig...", underline=0)
+		self.menuBar.add_cascade(label="Config", underline=0, menu=self.menuConfig)	
+		#show Menu elements		
+		self.root.config(menu=self.menuBar)
+		self.frame.pack()
 		self.root.mainloop()
 	
 	
-	'''eventChangeMenuItem'''
-	def eventChangeMenu_BinExcelConverter(self):
-		#self.MenuChange()
+	
+	#[section]: Login/Logout template
+	#System Login/Logout function
+	def createLoginWindow(self):
+		#open window: login
+		self.frmLogin = Toplevel(self.root)
+		self.frmLogin.title("login: enter your info.")
+		self.frmLogin.geometry("250x150")
+		self.frameLogin = Frame(self.frmLogin) #create a Frame "frameLogin" into fraLogin
+		#frameLoginID: a Frame layout to clone "LoginID section"
+		self.tempLoginID = StringVar() #string variable:"tempLoginID", assign value into "textLoginID"
+		self.frameLoginID = Frame(self.frameLogin, pady=12)
+		self.frameLoginID.pack(side=TOP, fill=X)
+		self.lblLoginID = Label(self.frameLoginID, text="UserID:", width=10).grid(row=0, column=0)
+		self.textLoginID = Entry(self.frameLoginID, width=20, textvariable=self.tempLoginID).grid(row=0, column=1)
+		#self.textLoginID.focus_set()
+		#frameLoginPWD: a Frame layout to clone "LoginPWD section"
+		self.tempLoginPWD = StringVar() #string variable:"tempLoginPWD", assign value into "textLoginPWD"
+		self.frameLoginPWD = Frame(self.frameLogin, pady=12)
+		self.frameLoginPWD.pack(side=TOP, fill=X)
+		self.lblLoginPWD = Label(self.frameLoginPWD, text="Password:", width=10).grid(row=0, column=0)
+		self.textLoginPWD = Entry(self.frameLoginPWD, width=20, show="*", textvariable=self.tempLoginPWD).grid(row=0, column=1)
+		#frameLoginBTN: a Frame layout to clone "LoginBTN section"
+		self.frameLoginBTN = Frame(self.frameLogin)
+		self.frameLoginBTN.pack(side=TOP, fill=X)
+		self.btnLogin = Button(self.frameLoginBTN, text="Login", command=self.login)
+		self.btnLogin.grid(row=0, column=0, padx=100)
+		#frame pack/mainloop
+		self.frameLogin.pack()
+		self.frmLogin.mainloop()
+	#Login	
+	def login(self):
+		#hardcode ID/PWD:1234/1234
+		self.frmLogin.destroy()
+		#self.DBconnector = DBC.DatabaseConnector(self.tempLoginID.get(), self.tempLoginPWD.get()) #create an DB object
+		self.DBconnector = DBC.DatabaseConnector("DataConverter.db") #create an DB object under dbName="DataConverter.db"
+		STATUS = ""
+		if(self.DBconnector.validateUserLogin(self.tempLoginID.get(), self.tempLoginPWD.get())):
+			self.loginID = self.tempLoginID
+			self.Msg = "Login successfully!"
+			self.DBconnector.insertUserLoginLog("OK")
+			tkMessageBox.showinfo("OK", self.Msg)
+		else:
+			self.loginID = ""
+			self.Msg = "Try again!"
+			self.DBconnector.insertUserLoginLog("ERROR")
+			tkMessageBox.showinfo("Error", self.Msg)
+		'''if(self.tempLoginID.get()=="1234" and self.tempLoginPWD.get()=="1234"):
+			self.loginID = self.tempLoginID
+			self.Msg = "Login successfully!"
+			tkMessageBox.showinfo("OK", self.Msg)
+		else:
+			self.loginID = ""
+			self.Msg = "Try again!"
+			tkMessageBox.showinfo("Error", self.Msg)'''
+			
+	#Logout
+	def logout(self):
+		self.loginID = ""
+		self.Msg = "Logout successfully!"
+		tkMessageBox.showinfo("OK", self.Msg)
+		
+	
+	
+	#[section]: open Files dialog
+	#File open
+	def browseFilePath(self, label):
+		tempPath = tkFileDialog.askopenfilename().encode('utf-8')
+		setattr(self, label, tempPath)
+		self.Msg = "Upload " + label + " successfully below:\n" + tempPath
+		tkMessageBox.showinfo("OK", self.Msg)
+		
+		
+		
+	#[section]: Menu "Tool" change event 
+	#Bin/Excel self.converter
+	def eventChangeMenuItem_Tool_BinExcelConverter(self):
+		if(self.loginID == ""):
+			tkMessageBox.showinfo("Error", "Please login system firstly!")
+		else:
+			self.menuAction = "Bin/Excel converter"
+			self.converter = BEC.BinExcelConverter(self.sourcePath, self.templatePath)
+			#OverRide object("self.converter") attribute
+			if(self.configExcelSetting is not ""):
+				self.converter.configExcelSetting = self.configExcelSetting
+				self.converter.defaultProgramSetting()
+			#start self.converter
+			if(not self.converter.validationEncapsulation()):
+				self.converter.writeLog()
+				tkMessageBox.showinfo("Error", self.converter.message)
+			else:			
+				self.converter.convertingEncapsulation()#start converting to a new file.
+				self.DBconnector.insertUserActionLog(self.menuAction, self.converter.backupFolder, self.converter.sourceFileName + "." + self.converter.sourceFileExtension, self.converter.backupFolder, self.converter.templateFileName + "." + self.converter.templateFileExtension, os.getcwd() + "\\resultData\\", self.converter.resultFolder.split('\\')[-1], self.converter.convertingItem, self.converter.configExcelSetting, "", "", "OK", "")
+				print self.converter.resultFolder
+				tkMessageBox.showinfo("OK", self.converter.message)
 
-		#frameSource=inputPath section
-		frameSource = Frame(self.root, width=400, height=50, padx=70, pady=10)
-		frameSource.pack(side=TOP, fill=X)
-		lblSource = Label(frameSource, text="Source:").grid(row=0, column=0)
-		textInputPath = Entry(frameSource, bd=1).grid(row=0, column=1)
-		btnSource = Button(frameSource, text="Browse", anchor=W, justify=LEFT, padx=5, command=lambda:textInputPath.set(tkFileDialog.askopenfilename().encode('utf-8'))).grid(row=0, column=2)
-		#btnSource = Button(frameSource, text="Browse", anchor=W, justify=LEFT, padx=5, command=lambda:self.getInputPath()).grid(row=0, column=2)
-		#tkFileDialog.askopenfilename().encode('utf-8')
-		
-		#frameTemplate=templatePath section
-		frameTemplate = Frame(self.root, width=400, height=50, padx=55, pady=10)
-		frameTemplate.pack(side=TOP, fill=X)
-		lblTemplate = Label(frameTemplate, text="Template:").grid(row=0, column=0)
-		self.textTemplatePath = Entry(frameTemplate, bd=1).grid(row=0, column=1)
-		btnTemplate = Button(frameTemplate, text="Browse", anchor=W, justify=LEFT, padx=5, command=lambda:self.getTemplatePath()).grid(row=0, column=2)
-		
-		#frameMessage=Message:
-		frameMessage = Frame(self.root, width=400, height=50, padx=35, pady=10)
-		frameMessage.pack(side=TOP, fill=X)
-		lblMessage = Label(frameMessage, text="Message:", anchor=W, justify=LEFT, padx=20, pady=20).grid(row=2, column=0)
-		#textMessage = ScrolledText(frameMessage, bd=1)
-		#textMessage.pack(row=2, column=1)
-		
-		#frameConvert=button Convert:
-		frameConvert = Frame(self.root, width=400, height=25, padx=150)
-		frameConvert.pack(side=TOP, fill=X)		
-		btnConverter = Button(frameConvert, text="Converter", command=lambda:self.convert(i, o))
-		btnConverter.grid(row=3, column=1)		
 
+
+
+				
+	#[section]: Menu "Config" change event
+	#"ExcelSetting"
+	def eventChhangeMenuItem_Config_ExcelSetting(self, item):
+		self.configExcelSetting = item
 		
 	
-	def getInputPath(self):
-		aaa = "1"
-		#self.tempInputPath.set(tkFileDialog.askopenfilename().encode('utf-8'))
-		#tempInputPath = tkFileDialog.askopenfilename().encode('utf-8')
-		#self.textInputPath.set(tempInputPath)
 	
-	def getTemplatePath(self):
-		tempTemplatePath = tkFileDialog.askopenfilename().encode('utf-8')
-		#self.TemplatePath.set(tempTemplatePath)
+
 	
+
 	
-	def convert(self, i, o):
-		self.i = i
-		self.o = o
-		print i	
-	
-	
-	def MenuChange(self):
-		#self.root.destroy()
-		print "1234"
-	
-	
-	
-	
-	
-	
-	
-	
-		
-		
+
+#default python menu_new.py to new an object from class "MainMenu()"
 if __name__ == '__main__':
-	print "5678"
-	
-	
-	
-
-	
+	root = Tk()
+	app = MainMenu(root)
+	app.pack()
+	root.mainloop()
 	
